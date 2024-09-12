4bf7454e339047acc604f46753fcec822ebfab25
Edvin <97972606+Eduyt17@users.noreply.github.com>
2024-09-11 22:29:03 -0600

    Update Tarea3Unidad2.py

---
+++ Tarea3Unidad2.py
@@ -0,0 +1,27 @@
+# Sección 1: Información de un libro favorito
+print("Título: El Principito")
+print("Autor: Antoine de Saint-Exupéry")
+print("Año de publicación: 1943")
+print("Género: Novela, Fábula")
+print("Número de páginas: 96")
+
+# Sección 2: Información de un país que deseas visitar
+pais = "Mónaco"
+capital = "Mónaco"
+idioma_oficial = "Francés"
+poblacion = 0.039  # En millones
+moneda = "Euro"
+
+print("\nInformación de un país que deseo visitar:")
+print("País:", pais)
+print("Capital:", capital)
+print("Idioma oficial:", idioma_oficial)
+print("Población (en millones):", poblacion)
+print("Moneda:", moneda)
+
+# Sección 3: Información de un personaje histórico favorito
+nombre_completo = "Leonardo da Vinci"
+fecha_nacimiento = "15 de abril de 1452"
+nacionalidad = "Italiana"
+ocupacion = "Polímata (pintor, ingeniero, inventor, científico)"
+logro_destacado = "Creación de obras maestras como La Mona Lisa y La Última Cena."
