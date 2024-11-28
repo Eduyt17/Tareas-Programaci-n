a2acb618be5604a040515224c2d10a0d0d0c9ec2
Edvin <97972606+Eduyt17@users.noreply.github.com>
2024-11-28 09:39:10 -0600

    Update unidad3.java

---
+++ Tarea6/unidad3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner input = new Scanner(System.in);
+    char []  palindromo;
+System.out.println("Ingresa el numero para analizar:");
+String n = input.nextLine();
+palindromo = n.toCharArray();
+
+int ultimaPosicion = n.length()-1;
+int cont1 = 0;
+int cont2 = ultimaPosicion;
+while(cont1<=cont2){
+    if (palindromo[cont1]==palindromo[cont2]){
+        cont1++;
+        cont2--;
+    }else {
+        System.out.println("Tu numero no es palindromo");
+    }if(cont1==cont2){
+        System.out.println("El numero es palindromo: ");
+        }
+        break;
+    }
+}
