a856875cfce12c1ce7f5246d4119ef2eb9d62b7f
Edvin <97972606+Eduyt17@users.noreply.github.com>
2024-11-28 09:35:48 -0600

    Update unidad3.java

---
+++ Tarea5/unidad3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int n = input.nextInt();
+        System.out.print("Los divisores son: ");
+        boolean esPrimo = true;
+        boolean primerDivisor = true;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                if (i != 1 && i != n) {
+                    esPrimo = false;
+                }
+                if (!primerDivisor) {
+                    System.out.print(", ");
+                }
+                System.out.print(i);
+                primerDivisor = false;
+            }
+        }
+        if (esPrimo) {
+            System.out.println("\nEl número " + n + " es primo");
+        }
+}
